89d5ad4df0055c4a885a3dc84e9e0273b48dafc3
Stacy Johns <stacykjohns@icloud.com>
2019-07-12 13:27:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,9 +3,7 @@ def find_element_index(array, value_to_find)
   array.length.times { |index|
   puts array[index]
 }
-=======
   array.index("value_to_find")
->>>>>>> 6a55a15ffc66756f243103b590ace2368909e9f8
 end
 
 def find_max_value(array)
